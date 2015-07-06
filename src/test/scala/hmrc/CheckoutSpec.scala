e58739be70dff6595edc68f31168efd3be41f500
Hraban Luyat <hraban@0brg.net>
2015-07-06 10:41:40 +0100

    Tests for deals (failing)

--- src/test/scala/hmrc/CheckoutSpec.scala
+++ src/test/scala/hmrc/CheckoutSpec.scala
@@ -6,9 +6,11 @@ import org.scalatest._
  * @author Hraban Luyat <hraban@0brg.net>
  */
 class CheckoutSpec extends FlatSpec with Matchers {
+  import Product._
+
   "Product enum" should "parse known strings as products" in {
-    Product("Apple") should be (Product.Apple)
-    Product("Orange") should be (Product.Orange)
+    Product("Apple") should be (Apple)
+    Product("Orange") should be (Orange)
   }
 
   it should "reject unknown product names" in {
@@ -26,14 +28,36 @@ class CheckoutSpec extends FlatSpec with Matchers {
   "A checkout system" should "compute prices correctly" in {
     val checkout = new Checkout(Pricing.standard)
     checkout.compute(List.empty[Product.Product]) should be (0)
-    checkout.compute(List(Product.Apple)) should be (60)
-    checkout.compute(List(Product.Orange)) should be (25)
-    checkout.compute(List(Product.Apple, Product.Apple, Product.Orange, Product.Apple)) should be (205)
+    checkout.compute(List(Apple)) should be (60)
+    checkout.compute(List(Orange)) should be (25)
+    checkout.compute(List(Apple, Apple, Orange, Apple)) should be (205)
   }
 
   it should "compute from a list of strings using map" in {
     val checkout = new Checkout(Pricing.standard)
     checkout.compute(List("Apple").map(Product(_))) should be (60)
     checkout.compute(List("Orange", "Apple", "Orange").view.map(Product(_))) should be (110)
+  }
+
+  it should "compute prices for deals correctly" in {
+    val checkout = new Checkout(Pricing.deal)
+    checkout.compute(List.empty[Product.Product]) should be (0)
+    checkout.compute(List(Apple)) should be (60)
+    checkout.compute(List(Apple, Apple)) should be (60)
+    checkout.compute(List(Apple, Apple, Apple)) should be (120)
+    checkout.compute(List(Apple, Apple, Apple, Apple)) should be (120)
+    checkout.compute(List(Orange)) should be (25)
+    checkout.compute(List(Orange, Orange)) should be (50)
+    checkout.compute(List(Orange, Orange, Orange)) should be (50)
+    checkout.compute(List(Orange, Orange, Orange, Orange)) should be (75)
+    checkout.compute(List(Orange, Orange, Orange, Orange, Orange)) should be (100)
+    checkout.compute(List(Orange, Orange, Orange, Orange, Orange, Orange)) should be (100)
+  }
+
+  it should "compute prices for deals correctly even when added out of order" in {
+    val checkout = new Checkout(Pricing.deal)
+    checkout.compute(List(Apple, Orange)) should be (85)
+    checkout.compute(List(Apple, Orange, Apple)) should be (85)
+    checkout.compute(List(Orange, Apple, Orange, Apple, Orange)) should be (110)
   }
 }
